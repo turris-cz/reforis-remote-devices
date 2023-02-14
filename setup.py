d9ac706fcb8f0a0585ed2916b5afcb5a96f11cdb
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2023-02-14 12:59:48 +0100

    Makefile: Restructure & update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+#  Copyright (C) 2019-2023 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -68,7 +68,7 @@ setuptools.setup(
         'Framework :: Flask',
         'Intended Audience :: Developers',
         'Development Status :: 3 - Alpha',
-        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
