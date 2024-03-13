3bf1218bc3f664623d112da74b76a0ed35e8b7e8
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-13 15:11:02 +0100

    Format Python files with Ruff

--- reforis_remote_devices/__init__.py
+++ reforis_remote_devices/__init__.py
@@ -3,7 +3,7 @@
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
 
-""" reForis Remote Devices plugin """
+"""reForis Remote Devices plugin"""
 
 from pathlib import Path
 from http import HTTPStatus
@@ -15,97 +15,92 @@ from flask_babel import gettext as _
 from reforis.foris_controller_api.utils import validate_json, APIError
 
 blueprint = Blueprint(
-    'RemoteDevices',
+    "RemoteDevices",
     __name__,
-    url_prefix='/remote-devices/api',
+    url_prefix="/remote-devices/api",
 )
 
 BASE_DIR = Path(__file__).parent
 
 remote_devices = {
-    'blueprint': blueprint,
-    # Define {python_module_name}/js/app.min.js
-    # See https://gitlab.labs.nic.cz/turris/reforis/reforis-distutils/blob/master/reforis_distutils/__init__.py#L11
-    'js_app_path': 'reforis_remote_devices/js/app.min.js',
-    'translations_path': BASE_DIR / 'translations',
+    "blueprint": blueprint,
+    "js_app_path": "reforis_remote_devices/js/app.min.js",
+    "translations_path": BASE_DIR / "translations",
 }
 
 
-@blueprint.route('/devices', methods=['GET'])
+@blueprint.route("/devices", methods=["GET"])
 def get_devices():
-    """ Get list of devices """
-    return jsonify(current_app.backend.perform('subordinates', 'list')['subordinates'])
+    """Get list of devices"""
+    return jsonify(current_app.backend.perform("subordinates", "list")["subordinates"])
 
 
-@blueprint.route('/devices', methods=['POST'])
+@blueprint.route("/devices", methods=["POST"])
 def post_devices():
-    """ Add new device """
-    if 'token' not in request.files:
-        raise APIError(_('Missing data for \'token\' file.'), HTTPStatus.BAD_REQUEST)
-    token_file = request.files['token']
+    """Add new device"""
+    if "token" not in request.files:
+        raise APIError(_("Missing data for 'token' file."), HTTPStatus.BAD_REQUEST)
+    token_file = request.files["token"]
 
     response = current_app.backend.perform(
-        'subordinates',
-        'add_sub',
-        {'token': base64.b64encode(token_file.read()).decode('utf-8')},
+        "subordinates",
+        "add_sub",
+        {"token": base64.b64encode(token_file.read()).decode("utf-8")},
     )
-    if response.get('result') is not True:
+    if response.get("result") is not True:
         raise APIError(
-            _('Cannot add remote device token. Check if device is already added.'),
+            _("Cannot add remote device token. Check if device is already added."),
             HTTPStatus.INTERNAL_SERVER_ERROR,
         )
 
     return jsonify(response), HTTPStatus.CREATED
 
 
-@blueprint.route('/devices/<controller_id>', methods=['DELETE'])
+@blueprint.route("/devices/<controller_id>", methods=["DELETE"])
 def delete_client(controller_id):
-    """ Delete device """
-    response = current_app.backend.perform('subordinates', 'del', {'controller_id': controller_id})
-    if response.get('result') is not True:
-        raise APIError(_('Cannot delete device.'), HTTPStatus.INTERNAL_SERVER_ERROR)
-    return '', HTTPStatus.NO_CONTENT
+    """Delete device"""
+    response = current_app.backend.perform("subordinates", "del", {"controller_id": controller_id})
+    if response.get("result") is not True:
+        raise APIError(_("Cannot delete device."), HTTPStatus.INTERNAL_SERVER_ERROR)
+    return "", HTTPStatus.NO_CONTENT
 
 
-@blueprint.route('/devices/<controller_id>', methods=['PATCH'])
+@blueprint.route("/devices/<controller_id>", methods=["PATCH"])
 def patch_devices(controller_id):
     """
     Change device options and its enabled state (either must be provided).
     """
     validate_json(request.json)
 
-    options = request.json.get('options')
-    enabled = request.json.get('enabled')
+    options = request.json.get("options")
+    enabled = request.json.get("enabled")
 
     if not options and enabled is None:
         raise APIError(_('Missing data for "options" or "enabled" field.'), HTTPStatus.BAD_REQUEST)
 
     responses = []
     if options:
-        custom_name = options.get('custom_name')
+        custom_name = options.get("custom_name")
         if custom_name and len(custom_name) > 30:
-            raise APIError(
-                _('Name is too long. Maximum 30 characters are allowed.'),
-                HTTPStatus.BAD_REQUEST
-            )
+            raise APIError(_("Name is too long. Maximum 30 characters are allowed."), HTTPStatus.BAD_REQUEST)
 
         options_response = current_app.backend.perform(
-            'subordinates',
-            'update_sub',
-            {'controller_id': controller_id, 'options': options},
+            "subordinates",
+            "update_sub",
+            {"controller_id": controller_id, "options": options},
         )
         responses.append(options_response)
     if enabled is not None:  # because "enabled" is boolean
         enabled_response = current_app.backend.perform(
-            'subordinates',
-            'set_enabled',
-            {'controller_id': controller_id, 'enabled': enabled},
+            "subordinates",
+            "set_enabled",
+            {"controller_id": controller_id, "enabled": enabled},
         )
         responses.append(enabled_response)
 
-    update_failed = any(response.get('result') is not True for response in responses)
+    update_failed = any(response.get("result") is not True for response in responses)
 
     if update_failed:
-        raise APIError(_('Cannot update device.'), HTTPStatus.INTERNAL_SERVER_ERROR)
+        raise APIError(_("Cannot update device."), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    return '', HTTPStatus.NO_CONTENT
+    return "", HTTPStatus.NO_CONTENT
--- tests/test_devices.py
+++ tests/test_devices.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#  Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -10,94 +10,71 @@ import pytest
 
 from reforis.test_utils import mock_backend_response
 
-DEVICES_URL = '/remote-devices/api/devices'
-DEVICE_URL = f'{DEVICES_URL}/1234'
+DEVICES_URL = "/remote-devices/api/devices"
+DEVICE_URL = f"{DEVICES_URL}/1234"
 
 
-@mock_backend_response({'subordinates': {'list': {'subordinates': []}}})
+@mock_backend_response({"subordinates": {"list": {"subordinates": []}}})
 def test_get_devices(client):
-    backend_response = {'subordinates': []}
+    backend_response = {"subordinates": []}
     response = client.get(DEVICES_URL)
     assert response.status_code == HTTPStatus.OK
-    assert response.json == backend_response['subordinates']
+    assert response.json == backend_response["subordinates"]
 
 
-@mock_backend_response({'subordinates': {'add_sub': {'result': True}}})
+@mock_backend_response({"subordinates": {"add_sub": {"result": True}}})
 def test_post_device(client):
     response = client.post(
-        DEVICES_URL,
-        data={"token": (BytesIO(b'foo=bar'), 'token.tar.gz')},
-        content_type='multipart/form-data'
+        DEVICES_URL, data={"token": (BytesIO(b"foo=bar"), "token.tar.gz")}, content_type="multipart/form-data"
     )
     assert response.status_code == HTTPStatus.CREATED
-    assert response.json == {'result': True}
+    assert response.json == {"result": True}
 
 
-@mock_backend_response({'subordinates': {'add_sub': {'result': False}}})
+@mock_backend_response({"subordinates": {"add_sub": {"result": False}}})
 def test_post_device_backend_error(client):
     response = client.post(
-        DEVICES_URL,
-        data={"token": (BytesIO(b'foo=bar'), 'token.tar.gz')},
-        content_type='multipart/form-data'
+        DEVICES_URL, data={"token": (BytesIO(b"foo=bar"), "token.tar.gz")}, content_type="multipart/form-data"
     )
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot add remote device token. Check if device is already added.'
+    assert response.json == "Cannot add remote device token. Check if device is already added."
 
 
-@mock_backend_response({'subordinates': {'add_sub': {}}})
+@mock_backend_response({"subordinates": {"add_sub": {}}})
 def test_post_device_missing_file(client):
-    response = client.post(
-        DEVICES_URL,
-        content_type='multipart/form-data'
-    )
+    response = client.post(DEVICES_URL, content_type="multipart/form-data")
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Missing data for \'token\' file.'
+    assert response.json == "Missing data for 'token' file."
 
 
-@mock_backend_response({'subordinates': {'del': {'result': True}}})
+@mock_backend_response({"subordinates": {"del": {"result": True}}})
 def test_delete_device(client):
     response = client.delete(DEVICE_URL)
     assert response.status_code == HTTPStatus.NO_CONTENT
 
 
-@mock_backend_response({'subordinates': {'del': {}}})
+@mock_backend_response({"subordinates": {"del": {}}})
 def test_delete_device_backend_error(client):
     response = client.delete(DEVICE_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot delete device.'
+    assert response.json == "Cannot delete device."
 
 
-@mock_backend_response({'subordinates': {'del': {'result': 1234}}})
+@mock_backend_response({"subordinates": {"del": {"result": 1234}}})
 def test_delete_device_unexpected_result(client):
     response = client.delete(DEVICE_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot delete device.'
+    assert response.json == "Cannot delete device."
 
 
-@mock_backend_response({
-    'subordinates': {
-        'set_enabled': {'result': True},
-        'update_sub': {'result': True}
-    }
-})
+@mock_backend_response({"subordinates": {"set_enabled": {"result": True}, "update_sub": {"result": True}}})
 def test_patch_device(client):
-    response = client.patch(DEVICE_URL, json={'enabled': False, 'options': {'custom_name': 'foobar'}})
+    response = client.patch(DEVICE_URL, json={"enabled": False, "options": {"custom_name": "foobar"}})
     assert response.status_code == HTTPStatus.NO_CONTENT
 
 
-@pytest.mark.parametrize(
-    'request_data',
-    [
-        {'enabled': True},
-        {'options': {'custom_name': 'foobar'}}
-    ]
-)
-@mock_backend_response({
-    'subordinates': {
-        'set_enabled': {'result': True},
-        'update_sub': {'result': True}
-    }
-})
+@pytest.mark.parametrize("request_data", [{"enabled": True}, {"options": {"custom_name": "foobar"}}])
+@mock_backend_response({"subordinates": {"set_enabled": {"result": True}, "update_sub": {"result": True}}})
 def test_patch_device_partially(client, request_data):
     response = client.patch(DEVICE_URL, json=request_data)
     assert response.status_code == HTTPStatus.NO_CONTENT
@@ -106,24 +83,24 @@ def test_patch_device_partially(client, request_data):
 def test_patch_device_invalid_json(client):
     response = client.patch(DEVICE_URL, json={})
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Invalid JSON'
+    assert response.json == "Invalid JSON"
 
 
 def test_patch_device_name_too_long(client):
-    response = client.patch(DEVICE_URL, json={'options': {'custom_name': 'Q' * 31}})
+    response = client.patch(DEVICE_URL, json={"options": {"custom_name": "Q" * 31}})
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Name is too long. Maximum 30 characters are allowed.'
+    assert response.json == "Name is too long. Maximum 30 characters are allowed."
 
 
 @pytest.mark.parametrize(
-    'backend_response',
+    "backend_response",
     [
-        {'subordinates': {'set_enabled': {'result': True}, 'update_sub': {'result': False}}},
-        {'subordinates': {'set_enabled': {'result': False}, 'update_sub': {'result': True}}}
-    ]
+        {"subordinates": {"set_enabled": {"result": True}, "update_sub": {"result": False}}},
+        {"subordinates": {"set_enabled": {"result": False}, "update_sub": {"result": True}}},
+    ],
 )
 def test_patch_device_backend_error(client, backend_response):
     with mock_backend_response(backend_response):
-        response = client.patch(DEVICE_URL, json={'enabled': False, 'options': {'custom_name': 'foobar'}})
+        response = client.patch(DEVICE_URL, json={"enabled": False, "options": {"custom_name": "foobar"}})
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot update device.'
+    assert response.json == "Cannot update device."
